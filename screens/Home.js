94314e3d779d49b05db251be8560292ccace332f
Juan David Giraldo <juandavidxaax@gmail.com>
2024-08-23 00:13:08 -0500

    Changes Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,140 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, Modal, FlatList, Image, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, TextInput, Modal, FlatList, Image, Alert, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../components/stylesHome';
 
+const products = [
+  {
+    id: '1',
+    name: 'Estuche del Galaxy Ultra',
+    price: 40.0,
+    description: 'Protege tu Galaxy Ultra con estilo y seguridad con este estuche premium.',
+    image: require('../assets/ProductosCelucos/Estuchegalaxy.png'),
+    categoria: 'Celulares',
+  },
+  {
+    id: '2',
+    name: 'Galaxy 23 Ultra',
+    price: 3200.0,
+    description: 'El smartphone más avanzado de Samsung, con cámara de alta resolución y rendimiento ultra rápido.',
+    image: require('../assets/ProductosCelucos/galaxy23ultra.png'),
+    categoria: 'Celulares',
+  },
+  {
+    id: '3',
+    name: 'Iphone 15',
+    price: 3000.0,
+    description: 'Descubre la nueva generación del iPhone con su diseño elegante y potente tecnología.',
+    image: require('../assets/ProductosCelucos/iphone15.png'),
+    categoria: 'Celulares',
+  },
+  {
+    id: '4',
+    name: 'Asus 14',
+    price: 2400.0,
+    description: 'Laptop compacta y eficiente, ideal para el trabajo y el entretenimiento diario.',
+    image: require('../assets/ProductosComp/asus14.png'),
+    categoria: 'Computadores',
+  },
+  {
+    id: '5',
+    name: 'Computador Gaming',
+    price: 5000.0,
+    description: 'Máquina de alto rendimiento para los gamers más exigentes, con gráficos y velocidad excepcionales.',
+    image: require('../assets/ProductosComp/compugaming.png'),
+    categoria: 'Computadores',
+  },
+  {
+    id: '6',
+    name: 'Ideapad 5',
+    price: 1900.0,
+    description: 'Laptop versátil con un diseño delgado y batería de larga duración.',
+    image: require('../assets/ProductosComp/Ideapad-5.png'),
+    categoria: 'Computadores',
+  },
+  {
+    id: '7',
+    name: 'Balón de Baloncesto',
+    price: 120.0,
+    description: 'Balón oficial de baloncesto, perfecto para competiciones y entrenamientos.',
+    image: require('../assets/ProductosDeportes/balon-baloncesto-oficial.png'),
+    categoria: 'Deportes',
+  },
+  {
+    id: '8',
+    name: 'Balón de Fútbol',
+    price: 200.0,
+    description: 'Balón de fútbol de alta calidad, diseñado para un rendimiento óptimo en el campo.',
+    image: require('../assets/ProductosDeportes/balon-futbol.png'),
+    categoria: 'Deportes',
+  },
+  {
+    id: '9',
+    name: 'Camisa de la Cabra',
+    price: 9000.0,
+    description: 'Edición limitada de la camisa inspirada en el icónico "GOAT" del deporte.',
+    image: require('../assets/ProductosDeportes/camisathegoat.png'),
+    categoria: 'Deportes',
+  },
+  {
+    id: '10',
+    name: 'Cama',
+    price: 2100.0,
+    description: 'Cama cómoda y moderna para un descanso placentero.',
+    image: require('../assets/ProductosHogar/cama.png'),
+    categoria: 'AccesoriosDeCasa',
+  },
+  {
+    id: '11',
+    name: 'Comedor',
+    price: 1300.0,
+    description: 'Juego de comedor elegante y funcional para cualquier hogar.',
+    image: require('../assets/ProductosHogar/comedor.png'),
+    categoria: 'AccesoriosDeCasa',
+  },
+  {
+    id: '12',
+    name: 'Lámpara de Noche',
+    price: 600.0,
+    description: 'Lámpara de mesa con un diseño moderno, ideal para iluminar cualquier espacio.',
+    image: require('../assets/ProductosHogar/lampara-de-mesa.png'),
+    categoria: 'AccesoriosDeCasa',
+  },
+  {
+    id: '13',
+    name: 'Play 5',
+    price: 2600.0,
+    description:
+      'La consola de videojuegos más avanzada de Sony, con gráficos impresionantes y un rendimiento superior.',
+    image: require('../assets/ProductosConsolas/play-5.png'),
+    categoria: 'Games',
+  },
+  {
+    id: '14',
+    name: 'Play 2',
+    price: 400.0,
+    description: 'Consola retro de Sony, perfecta para los amantes de los juegos clásicos.',
+    image: require('../assets/ProductosConsolas/play-2.png'),
+    categoria: 'Games',
+  },
+  {
+    id: '15',
+    name: 'Xbox 360',
+    price: 550.0,
+    description: 'La clásica consola de Microsoft, conocida por su catálogo de juegos y rendimiento.',
+    image: require('../assets/ProductosConsolas/xbox-360.png'),
+    categoria: 'Games',
+  },
+];
+export { products }; 
 const Home = () => {
   const navigation = useNavigation();
   const [isMenuVisible, setMenuVisible] = useState(false);
   const [cart, setCart] = useState([]);
+  const [favorites, setFavorites] = useState([]); // Estado para favoritos
+  const [searchText, setSearchText] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   const toggleMenu = () => setMenuVisible(!isMenuVisible);
 
@@ -17,129 +144,6 @@ const Home = () => {
     { name: 'Lista de artículos', screen: 'ListProducts' },
     { name: 'Sucursal de pago', screen: 'PaymentBranch' },
     { name: 'Ayuda y Soporte', screen: 'HelpSupport' },
-  ];
-  const products = [
-    {
-      id: '1',
-      name: 'Estuche del Galaxy Ultra',
-      price: 40.0,
-      description: 'Protege tu Galaxy Ultra con estilo y seguridad con este estuche premium.',
-      image: require('../assets/ProductosCelucos/Estuchegalaxy.png'),
-      categoria: 'Celulares',
-    },
-    {
-      id: '2',
-      name: 'Galaxy 23 Ultra',
-      price: 3200.0,
-      description: 'El smartphone más avanzado de Samsung, con cámara de alta resolución y rendimiento ultra rápido.',
-      image: require('../assets/ProductosCelucos/galaxy23ultra.png'),
-      categoria: 'Celulares',
-    },
-    {
-      id: '3',
-      name: 'Iphone 15',
-      price: 3000.0,
-      description: 'Descubre la nueva generación del iPhone con su diseño elegante y potente tecnología.',
-      image: require('../assets/ProductosCelucos/iphone15.png'),
-      categoria: 'Celulares',
-    },
-    {
-      id: '4',
-      name: 'Asus 14',
-      price: 2400.0,
-      description: 'Laptop compacta y eficiente, ideal para el trabajo y el entretenimiento diario.',
-      image: require('../assets/ProductosComp/asus14.png'),
-      categoria: 'Computadores',
-    },
-    {
-      id: '5',
-      name: 'Computador Gaming',
-      price: 5000.0,
-      description: 'Máquina de alto rendimiento para los gamers más exigentes, con gráficos y velocidad excepcionales.',
-      image: require('../assets/ProductosComp/compugaming.png'),
-      categoria: 'Computadores',
-    },
-    {
-      id: '6',
-      name: 'Ideapad 5',
-      price: 1900.0,
-      description: 'Laptop versátil con un diseño delgado y batería de larga duración.',
-      image: require('../assets/ProductosComp/Ideapad-5.png'),
-      categoria: 'Computadores',
-    },
-    {
-      id: '7',
-      name: 'Balón de Baloncesto',
-      price: 120.0,
-      description: 'Balón oficial de baloncesto, perfecto para competiciones y entrenamientos.',
-      image: require('../assets/ProductosDeportes/balon-baloncesto-oficial.png'),
-      categoria: 'Deportes',
-    },
-    {
-      id: '8',
-      name: 'Balón de Fútbol',
-      price: 200.0,
-      description: 'Balón de fútbol de alta calidad, diseñado para un rendimiento óptimo en el campo.',
-      image: require('../assets/ProductosDeportes/balon-futbol.png'),
-      categoria: 'Deportes',
-    },
-    {
-      id: '9',
-      name: 'Camisa de la Cabra',
-      price: 9000.0,
-      description: 'Edición limitada de la camisa inspirada en el icónico "GOAT" del deporte.',
-      image: require('../assets/ProductosDeportes/camisathegoat.png'),
-      categoria: 'Deportes',
-    },
-    {
-      id: '10',
-      name: 'Cama',
-      price: 2100.0,
-      description: 'Cama cómoda y moderna para un descanso placentero.',
-      image: require('../assets/ProductosHogar/cama.png'),
-      categoria: 'AccesoriosDeCasa',
-    },
-    {
-      id: '11',
-      name: 'Comedor',
-      price: 1300.0,
-      description: 'Juego de comedor elegante y funcional para cualquier hogar.',
-      image: require('../assets/ProductosHogar/comedor.png'),
-      categoria: 'AccesoriosDeCasa',
-    },
-    {
-      id: '12',
-      name: 'Lámpara de Noche',
-      price: 600.0,
-      description: 'Lámpara de mesa con un diseño moderno, ideal para iluminar cualquier espacio.',
-      image: require('../assets/ProductosHogar/lampara-de-mesa.png'),
-      categoria: 'AccesoriosDeCasa',
-    },
-    {
-      id: '13',
-      name: 'Play 5',
-      price: 2600.0,
-      description:
-        'La consola de videojuegos más avanzada de Sony, con gráficos impresionantes y un rendimiento superior.',
-      image: require('../assets/ProductosConsolas/play-5.png'),
-      categoria: 'AccesoriosDeCasa',
-    },
-    {
-      id: '14',
-      name: 'Play 2',
-      price: 400.0,
-      description: 'Consola retro de Sony, perfecta para los amantes de los juegos clásicos.',
-      image: require('../assets/ProductosConsolas/play-2.png'),
-      categoria: 'AccesoriosDeCasa',
-    },
-    {
-      id: '15',
-      name: 'Xbox 360',
-      price: 550.0,
-      description: 'La clásica consola de Microsoft, conocida por su catálogo de juegos y rendimiento.',
-      image: require('../assets/ProductosConsolas/xbox-360.png'),
-      categoria: 'AccesoriosDeCasa',
-    },
   ];
   
   useEffect(() => {
@@ -173,30 +177,35 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <View style={styles.accountSection}>
-        <Pressable onPress={() => navigation.navigate('UserProfile')}>
+        <TouchableOpacity onPress={() => navigation.navigate('UserProfile')}>
           <Ionicons name="person-circle" size={40} color="black" />
-        </Pressable>
+        </TouchableOpacity>
         <Text style={styles.accountText}>Mi Cuenta</Text>
-        <Pressable style={styles.menuButton} onPress={toggleMenu}>
+        <TouchableOpacity style={styles.menuButton} onPress={toggleMenu}>
           <Ionicons name="menu" size={30} color="black" />
-        </Pressable>
+        </TouchableOpacity>
       </View>
 
-      <TouchableOpacity style={styles.searchBar} onPress={() => navigation.navigate('SearchScreen')}>
+      <View style={styles.searchBar}>
         <Ionicons name="search" size={20} color="gray" />
-        <TextInput placeholder="Buscar productos..." style={styles.searchInput} />
-      </TouchableOpacity>
+        <TextInput
+          placeholder="Buscar productos..."
+          style={styles.searchInput}
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+      </View>
 
       <View style={styles.iconRow}>
-        <Pressable onPress={() => navigation.navigate('Favorites')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Favorites')}>
           <Ionicons name="heart" size={30} color="red" />
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate('ShoppingCart', { cart })}>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('ShoppingCart', { cart })}>
           <Ionicons name="cart" size={30} color="black" />
-        </Pressable>
-        <Pressable onPress={() => navigation.navigate('ProductDetail', { products })}>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('ProductDetail', { products })}>
           <Ionicons name="information-circle" size={30} color="blue" />
-        </Pressable>
+        </TouchableOpacity>
       </View>
 
       <FlatList
@@ -231,7 +240,7 @@ const Home = () => {
               data={menuItems}
               keyExtractor={(item) => item.name}
               renderItem={({ item }) => (
-                <Pressable
+                <TouchableOpacity
                   style={styles.menuItem}
                   onPress={() => {
                     navigation.navigate(item.screen);
@@ -239,7 +248,7 @@ const Home = () => {
                   }}
                 >
                   <Text style={styles.menuItemText}>{item.name}</Text>
-                </Pressable>
+                </TouchableOpacity>
               )}
             />
           </View>
